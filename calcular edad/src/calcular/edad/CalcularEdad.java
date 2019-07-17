764bbf13a3aaff4bdc0192125f596c9fea804be1
Joffre1714014741 <52941753+Joffre1714014741@users.noreply.github.com>
2019-07-17 00:08:06 -0500

    Update CalcularEdad.java

--- calcular edad/src/calcular/edad/CalcularEdad.java
+++ calcular edad/src/calcular/edad/CalcularEdad.java
@@ -8,7 +8,7 @@ public class CalcularEdad {
  
 	public static void main(String[] args) {
 		
-		System.out.println(calcular(new GregorianCalendar(1984,02,14)));
+		System.out.println(calcular(new GregorianCalendar(1994,02,14)));
 	}
  
     public static int calcular(Calendar fechaNac) {
